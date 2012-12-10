687d9efd2c8323025872bbf2e0f4812aadd4c2e8
Secretariat1987 <mixey.viti@bigmir.net>
2012-12-10 23:27:51 +0200

    20121210_start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Citrusgraph::Application.config.secret_token = '1a04eec80fb9b7d7e918463fd5707b59202f83d5b18f3942cd1f0bcedc247e944722aad13bbb30d4afa4d8443c3c6e2020029bfb109ac58ec4ff90d3dd2272e2'
